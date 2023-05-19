7640d4fbb50584568df504fae12a2dc06b2abec0
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-05-19 11:48:42 -0400

    Add project files.

---
+++ WazeCredit_LearnDependencyInjection/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WazeCredit_LearnDependencyInjection.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
